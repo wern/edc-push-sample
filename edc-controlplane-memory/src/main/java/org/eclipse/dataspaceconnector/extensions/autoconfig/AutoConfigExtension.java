9e7808e3cebb4539d861d94b9b9a55591d994e2d
Werner Eberling <werner.eberling@mathema.de>
2022-08-25 17:19:50 +0200

    Get access token from IDP

--- edc-controlplane-memory/src/main/java/org/eclipse/dataspaceconnector/extensions/autoconfig/AutoConfigExtension.java
+++ edc-controlplane-memory/src/main/java/org/eclipse/dataspaceconnector/extensions/autoconfig/AutoConfigExtension.java
@@ -1,11 +1,18 @@
 package org.eclipse.dataspaceconnector.extensions.autoconfig;
 
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+
 import org.eclipse.dataspaceconnector.dataloading.AssetLoader;
+import org.eclipse.dataspaceconnector.extensions.autoconfig.service.OAuthLoginService;
 import org.eclipse.dataspaceconnector.policy.model.Action;
 import org.eclipse.dataspaceconnector.policy.model.Permission;
 import org.eclipse.dataspaceconnector.policy.model.Policy;
 import org.eclipse.dataspaceconnector.spi.asset.AssetSelectorExpression;
 import org.eclipse.dataspaceconnector.spi.contract.offer.store.ContractDefinitionStore;
+import org.eclipse.dataspaceconnector.spi.monitor.Monitor;
 import org.eclipse.dataspaceconnector.spi.policy.store.PolicyStore;
 import org.eclipse.dataspaceconnector.spi.system.Inject;
 import org.eclipse.dataspaceconnector.spi.system.ServiceExtension;
@@ -13,6 +20,8 @@ import org.eclipse.dataspaceconnector.spi.system.ServiceExtensionContext;
 import org.eclipse.dataspaceconnector.spi.types.domain.DataAddress;
 import org.eclipse.dataspaceconnector.spi.types.domain.asset.Asset;
 import org.eclipse.dataspaceconnector.spi.types.domain.contract.offer.ContractDefinition;
+
+import okhttp3.OkHttpClient;
 
 public class AutoConfigExtension implements ServiceExtension {
 
@@ -22,6 +31,10 @@ public class AutoConfigExtension implements ServiceExtension {
     private static final String PCF_ENDPOINT = "pcf.asset.endpoint";
     private static final String AUTH_KEY = "pcf.asset.auth.key";
     private static final String AUTH_CODE = "pcf.asset.auth.code";
+    private static final String IDP_URL = "pcf.asset.auth.oidc.idp.url";
+    private static final String CLIENT_ID = "pcf.asset.auth.oidc.client.id";
+    private static final String CLIENT_SECRET = "pcf.asset.auth.oidc.client.secret";
+
 
     @Inject
     private ContractDefinitionStore contractStore;
@@ -29,18 +42,62 @@ public class AutoConfigExtension implements ServiceExtension {
     private AssetLoader loader;
     @Inject
     private PolicyStore policyStore;
+    @Inject
+    private OkHttpClient httpClient;
 
+    private Monitor monitor;
+
+    private ServiceExtensionContext context;
+
+    private final ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
+    
+    private ScheduledFuture<?> schedulerHandle;
+
+    private OAuthLoginService loginService;
 
     public void initialize(ServiceExtensionContext context) {
-        var monitor = context.getMonitor();
-
+        this.monitor = context.getMonitor();
+        this.context = context;
+        loginService = new OAuthLoginService(monitor, httpClient);
+        var authCode = context.getSetting(AUTH_CODE, "");
         var policy = createPolicy(context);
         policyStore.save(policy);
 
-        registerDataEntries(context);
+        registerDataEntries(context, authCode, false);
         registerContractDefinition(policy.getUid());
 
         monitor.info("AutoRegisterAssetExtension initialized, Assets registered.");
+    }
+
+    public void start() {
+        monitor.info("AutoRegisterAssetExtension starting... ");
+        final Runnable assetUpdater = new Runnable() {
+            public void run() { 
+                monitor.debug("Trying to refresh asset...");
+                var idpUrl = context.getSetting(IDP_URL, "");
+                var clientId = context.getSetting(CLIENT_ID, "");
+                var clientSecret = context.getSetting(CLIENT_SECRET, "");
+
+                if( idpUrl.isBlank() || clientId.isBlank() || clientSecret.isBlank()){
+                    monitor.info("No IDP information configured. Token refresh disabled!");
+                    return;
+                }
+
+                try{
+                    registerDataEntries(context, "Bearer " + loginService.getToken(idpUrl, clientId, clientSecret), true); 
+                }catch(Throwable t){
+                    monitor.severe("Something went wrong while recreationg the asset", t);
+                }
+                monitor.debug("Done!");
+            }
+         };
+         schedulerHandle =
+            scheduler.scheduleAtFixedRate(assetUpdater, 0, 60, TimeUnit.MINUTES);
+    }
+    
+    public void shutdown() {
+        monitor.info("AutoRegisterAssetExtension shutting down... ");
+        schedulerHandle.cancel(true);
     }
 
     private Policy createPolicy(ServiceExtensionContext context) {
@@ -60,15 +117,13 @@ public class AutoConfigExtension implements ServiceExtension {
                 .build();
     }
 
-    private void registerDataEntries(ServiceExtensionContext context) {
+    private void registerDataEntries(ServiceExtensionContext context, String authCode, boolean removeOldAsset) {
 
         var monitor = context.getMonitor();
 
         var endpointURL = context.getSetting(PCF_ENDPOINT, "not set!");
         var assetID = context.getSetting(ASSET_ID, "42424242424242");
         var authKey = context.getSetting(AUTH_KEY, "");
-        var authCode = context.getSetting(AUTH_CODE, "");
-        var stuff = context.getSetting("pcf.stuff", "");
 
         monitor.debug("Using endpoint '"+ endpointURL +"'");
         monitor.debug("Using authKey '"+ authKey +"'");
@@ -91,6 +146,7 @@ public class AutoConfigExtension implements ServiceExtension {
 
         var dataAddress = dataAddressBuilder.build();
 
+
         var asset = Asset.Builder.newInstance()
                 .id(assetID)
                 .name("PCF Exchange Asset")
@@ -99,6 +155,12 @@ public class AutoConfigExtension implements ServiceExtension {
                 .property("asset:prop:type", "pcf-push")
                 .build();
 
+        if(removeOldAsset){
+            monitor.debug("Removing old asset...");
+            loader.deleteById(asset.getId());
+        }
+
+        monitor.debug("Loading new asset...");
         loader.accept(asset, dataAddress);
     }
 
---
+++ edc-controlplane-memory/src/main/java/org/eclipse/dataspaceconnector/extensions/autoconfig/model/TokenResponse.java
@@ -0,0 +1,8 @@
+package org.eclipse.dataspaceconnector.extensions.autoconfig.model;
+
+public class TokenResponse {
+    public String access_token;
+	public String scope;
+	public String expires_in;
+	public String token_type;
+}
